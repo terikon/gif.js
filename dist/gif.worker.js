f91aef7ed57e5d75aa30b76224c840c9a7f1cfc0
viskin <r.viskin@terikon.com>
2016-09-26 13:51:58 +0300

    Added micro-optimization to critical functions

--- dist/gif.worker.js
+++ dist/gif.worker.js
@@ -360,7 +360,7 @@
 	  Index pixels, without dithering
 	  This method is most expensive (75% of time) because of calls to findClosestRGB, and findClosestRGB should be optimized.
 	*/
-	GIFEncoder.prototype.indexPixels = function(imgq) {
+	GIFEncoder.prototype.indexPixels = function() {
 	  var nPix = this.pixels.length / 3;
 	  this.indexedPixels = new Uint8Array(nPix);
 	  var k = 0;
